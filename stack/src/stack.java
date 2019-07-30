b0116c11f807c49bb446342dd4763a806ac9a31c
xlr10 <vjihovjiho@gmail.com>
2019-07-30 21:21:53 +0900

    stack #1 commit (readme.txt)

--- stack/src/stack.java
+++ stack/src/stack.java
@@ -1,2 +1,30 @@
 public class stack {
+
+    //최초 생성 : item은 int
+    //refactoring -> 자료형 무관
+
+    //    해당 item을 Stack의 top에 삽입
+    public static void push(int item){
+
+    }
+
+    //    Stack의 top에 있는 item을 삭제하고 해당 item을 반환
+    public static int pop(){
+        return 0;
+    }
+
+    //    Stack의 top에 있는 item을 삭제하지않고 해당 item을 반환
+    public static int peek(){
+        return 0;
+    }
+
+    //    Stack이 비어있으면 true를 반환 그렇지않으면 false를 반환
+    public static boolean empty(){
+        return false;
+    }
+
+    //    해당 Object의 위치를 반환 Stack의 top 위치는 1, 해당 Object가 없으면 -1을 반환
+    public static int search(Object o){
+        return 0;
+    }
 }
